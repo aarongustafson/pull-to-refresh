cea743afa375e03b46ca306776b6d0ff68d2f07d
Aaron Gustafson <aaron@easy-designs.net>
2025-12-08 09:07:10 -0800

    Harden define

--- define.js
+++ define.js
@@ -1,3 +1,18 @@
 import { PullToRefreshElement } from './pull-to-refresh.js';
 
-customElements.define('pull-to-refresh', PullToRefreshElement);
+export function definePullToRefresh(tagName = 'pull-to-refresh') {
+	const hasWindow = typeof window !== 'undefined';
+	const registry = hasWindow ? window.customElements : undefined;
+
+	if (!registry || typeof registry.define !== 'function') {
+		return false;
+	}
+
+	if (!registry.get(tagName)) {
+		registry.define(tagName, PullToRefreshElement);
+	}
+
+	return true;
+}
+
+definePullToRefresh();
